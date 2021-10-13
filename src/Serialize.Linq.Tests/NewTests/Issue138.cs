d0cb133d396e1a11c0923ac29f4d93fd388f9809
OA <oahrens@bluewin.ch>
2021-10-13 10:19:57 +0200

    New unit tests for issue #138.

---
+++ src/Serialize.Linq.Tests/NewTests/Issue138.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Serializers;
+
+namespace Serialize.Linq.Tests.NewTests
+{
+    /// <summary>
+    /// https://github.com/esskar/Serialize.Linq/issues/138
+    /// </summary>
+    [TestClass]
+    public class Issue138
+    {
+        [TestMethod]
+        public void SerializeInnerFunctionBinary()
+        {
+#pragma warning disable CS0618 // type or member is obsolete
+            var serializer = new ExpressionSerializer(new BinarySerializer());
+#pragma warning restore CS0618 // type or member is obsolete
+            Expression<Func<object, object>> expr = x => Functions.F(x);
+
+            var value = serializer.SerializeBinary(expr);
+            Expression<Func<object, object>> actualExpression = (Expression<Func<object, object>>)serializer.DeserializeBinary(value);
+
+            Assert.AreEqual(expr.ToString(), actualExpression.ToString());
+            Assert.AreEqual(expr.Compile().Invoke(2), actualExpression.Compile().Invoke(2));
+        }
+
+        [TestMethod]
+        public void SerializeInnerFunctionXml()
+        {
+#pragma warning disable CS0618 // type or member is obsolete
+            var serializer = new ExpressionSerializer(new XmlSerializer());
+#pragma warning restore CS0618 // type or member is obsolete
+            Expression<Func<object, object>> expr = x => Functions.F(x);
+
+            var value = serializer.SerializeText(expr);
+            Expression<Func<object, object>> actualExpression = (Expression<Func<object, object>>)serializer.DeserializeText(value);
+
+            Assert.AreEqual(expr.ToString(), actualExpression.ToString());
+            Assert.AreEqual(expr.Compile().Invoke(2), actualExpression.Compile().Invoke(2));
+        }
+
+        [TestMethod]
+        public void SerializeInnerFunctionJson()
+        {
+#pragma warning disable CS0618 // type or member is obsolete
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+#pragma warning restore CS0618 // type or member is obsolete
+            Expression<Func<object, object>> expr = x => Functions.F(x);
+
+            var value = serializer.SerializeText(expr);
+            Expression<Func<object, object>> actualExpression = (Expression<Func<object, object>>)serializer.DeserializeText(value);
+
+            Assert.AreEqual(expr.ToString(), actualExpression.ToString());
+            Assert.AreEqual(expr.Compile().Invoke(2), actualExpression.Compile().Invoke(2));
+        }
+    }
+
+    internal class Functions
+    {
+        public static object F(object x)
+        {
+            return x;
+        }
+    }
+}
---
+++ src/Serialize.Linq.Tests/NewTests/Issue138Generic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Interfaces;
+using Serialize.Linq.Serializers;
+
+namespace Serialize.Linq.Tests.NewTests
+{
+    /// <summary>
+    /// https://github.com/esskar/Serialize.Linq/issues/138
+    /// </summary>
+    [TestClass]
+    public class Issue138Generic
+    {
+        [TestMethod]
+        public void SerializeInnerFunction()
+        {
+            SerializeInnerFunctionInternal(new XmlSerializer());
+            SerializeInnerFunctionInternal(new BinarySerializer());
+            SerializeInnerFunctionInternal(new JsonSerializer());
+        }
+
+        private void SerializeInnerFunctionInternal<TSerialize>(IGenericSerializer<TSerialize> serializer)
+        {
+            Expression<Func<object, object>> expr = x => FunctionsGeneric.F(x);
+
+            var value = serializer.SerializeGeneric(expr);
+            Expression<Func<object, object>> actualExpression = (Expression<Func<object, object>>)serializer.DeserializeGeneric(value);
+
+            Assert.AreEqual(expr.ToString(), actualExpression.ToString());
+            Assert.AreEqual(expr.Compile().Invoke(2), actualExpression.Compile().Invoke(2));
+        }
+    }
+
+    internal class FunctionsGeneric
+    {
+        public static object F(object x)
+        {
+            return x;
+        }
+    }
+}
