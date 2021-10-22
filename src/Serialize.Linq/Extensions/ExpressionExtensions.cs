ecacf72c14b6faf8059ba54f65afeb0277b3d573
OA <oahrens@bluewin.ch>
2021-10-22 21:44:28 +0200

    Deleted not longer needed expressen extensions.

--- src/Serialize.Linq/Extensions/ExpressionExtensions.cs
+++ src/Serialize.Linq/Extensions/ExpressionExtensions.cs
@@ -187,76 +187,6 @@ namespace Serialize.Linq.Extensions
         }
 
         /// <summary>
-        /// Gets the link nodes of an expression tree.
-        /// </summary>
-        /// <param name="expression">The expression.</param>
-        /// <returns></returns>
-        internal static IEnumerable<Expression> GetLinkNodes(this Expression expression)
-        {
-            switch (expression)
-            {
-                case LambdaExpression lambdaExpression:
-                {
-                    yield return lambdaExpression.Body;
-                    foreach (var parameter in lambdaExpression.Parameters)
-                        yield return parameter;
-                    break;
-                }
-                case BinaryExpression binaryExpression:
-                    yield return binaryExpression.Left;
-                    yield return binaryExpression.Right;
-                    break;
-                case ConditionalExpression conditionalExpression:
-                    yield return conditionalExpression.IfTrue;
-                    yield return conditionalExpression.IfFalse;
-                    yield return conditionalExpression.Test;
-                    break;
-                case InvocationExpression invocationExpression:
-                {
-                    yield return invocationExpression.Expression;
-                    foreach (var argument in invocationExpression.Arguments)
-                        yield return argument;
-                    break;
-                }
-                case ListInitExpression listInitExpression:
-                    yield return listInitExpression.NewExpression;
-                    break;
-                case MemberExpression memberExpression:
-                    yield return memberExpression.Expression;
-                    break;
-                case MemberInitExpression memberInitExpression:
-                    yield return memberInitExpression.NewExpression;
-                    break;
-                case MethodCallExpression methodCallExpression:
-                {
-                    foreach (var argument in methodCallExpression.Arguments)
-                        yield return argument;
-                    if (methodCallExpression.Object != null)
-                        yield return methodCallExpression.Object;
-                    break;
-                }
-                case NewArrayExpression newArrayExpression:
-                {
-                    foreach (var item in newArrayExpression.Expressions)
-                        yield return item;
-                    break;
-                }
-                case NewExpression newExpression:
-                {
-                    foreach (var item in newExpression.Arguments)
-                        yield return item;
-                    break;
-                }
-                case TypeBinaryExpression typeBinaryExpression:
-                    yield return typeBinaryExpression.Expression;
-                    break;
-                case UnaryExpression unaryExpression:
-                    yield return unaryExpression.Operand;
-                    break;
-            }
-        }
-
-        /// <summary>
         /// From https://stackoverflow.com/a/31360768/2883733.
         /// </summary>
         public static string GetDebugView(this Expression expression)
@@ -272,32 +202,6 @@ namespace Serialize.Linq.Extensions
 
                 return info.GetValue(expression, null/* TODO Change to default(_) if this is not a reference type */) as string;
             }
-        }
-
-        /// <summary>
-        /// Gets the nodes of an expression tree.
-        /// </summary>
-        /// <param name="expression">The expression.</param>
-        /// <returns></returns>
-        internal static IEnumerable<Expression> GetNodes(this Expression expression)
-        {
-            foreach (var node in expression.GetLinkNodes())
-            {
-                foreach (var subNode in node.GetNodes())
-                    yield return subNode;
-            }
-            yield return expression;
-        }
-
-        /// <summary>
-        /// Gets the nodes of an expression tree of given expression type.
-        /// </summary>
-        /// <typeparam name="TExpression">The type of the expression.</typeparam>
-        /// <param name="expression">The expression.</param>
-        /// <returns></returns>
-        internal static IEnumerable<TExpression> GetNodes<TExpression>(this Expression expression) where TExpression : Expression
-        {
-            return expression.GetNodes().OfType<TExpression>();
         }
     }
 }
